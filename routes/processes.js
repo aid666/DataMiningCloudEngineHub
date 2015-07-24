ca470e948ab413e90223214459e522ec75611531
aid666 <aid666@gmail.com>
2015-07-25 01:42:26 +0800

    Add basic code for waiting queue, query check log and push appcept check log

--- routes/processes.js
+++ routes/processes.js
@@ -36,4 +36,63 @@ router.get('/:key/result', function(req, res, next) {
   });
 });
 
+
+router.get('/:pKey/checklogs', function(req, res, next) {
+  var pKey = req.params.pKey;
+  processMetadata.count({ procKey: pKey }, function (err, count) {
+    if( count > 0 ){
+      queue_check_logs.find({ procKey: pKey }, function (err, logs){
+        res.json(logs);
+      });
+    }else{
+      res.sendStatus(404);
+    }
+  });
+});
+
+router.post('/:pKey/checklogs', function(req, res, next) {
+  var pKey = req.params.pKey;
+  processMetadata.count({ procKey: pKey }, function (err, count) {
+    if( count > 0 ){
+      var resLog = req.body;
+      var checkResult = resLog == null ? null : resLog.status;
+
+      function insertLog(){
+        var newLog = {
+          procKey: pKey,
+          recordDatetime: new Date(),
+          checkDatetime: resLog.datetime,
+          status: checkResult,
+          description: resLog.description,
+          checkerKey: resLog.checkerKey
+        }
+        queue_check_logs.insert(newLog);
+        res.sendStatus(204);
+      }
+      if( checkResult == null ){
+          res.status(403).send('CheckResult can not be empty.');
+      }else if( checkResult == 'ACCEPT' ){
+        //Check the logs, only one ACCEPT can be insert into logs.
+        queue_check_logs.count({procKey: pKey, status: 'ACCEPT'}, function (err, count){
+          if( count == 0 ){
+            processMetadata.update({procKey: pKey, status: 'PENDING'}, { $set: {status: 'RUNNING'}}, {}, function(err, numReplaced){
+              if(err == null && numReplaced == 1){
+                //Update
+                insertLog();
+              }
+            })
+          }else{
+            res.status(403).send('Already ACCEPT by the dispatcher');
+          }
+        });
+      }else{
+        insertLog();
+      }
+
+    }else{
+      res.sendStatus(404);
+    }
+  });
+});
+
 module.exports = router;
--- routes/queue.js
+++ routes/queue.js
@@ -7,30 +7,4 @@ router.get('', function(req, res, next) {
   });
 });
 
-router.get('/:wpKey', function(req, res, next) {
-  var wpKey = req.params.wpKey;
-  processMetadata.find({ procKey: wpKey }, function (err, docs) {
-    if(docs.length > 0){
-      queue_check_logs.find({ procKey: wpKey }, function (err, logs){
-        res.json(logs);
-      });
-    }else{
-      res.sendStatus(404);
-    }
-  });
-});
-
-router.post('/:wpKey', function(req, res, next) {
-  var wpKey = req.params.wpKey;
-  processMetadata.find({ procKey: wpKey }, function (err, docs) {
-    if(docs.length > 0){
-      queue_check_logs.insert(req.params.body);
-      res.sendStatus(204);
-    }else{
-      res.sendStatus(404);
-    }
-  });
-});
-
-
 module.exports = router;
